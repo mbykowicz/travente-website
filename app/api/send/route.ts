69a2b0947fbefcaed5e00294dd3b35a1e47934c9
Mateusz Bykowicz <mbykowicz@gmail.com>
2023-11-11 23:39:14 +0100

    chore

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,26 +1,3 @@
-// import EmailTemplate from '@/components/emailTemplate'
-// import { NextResponse } from 'next/server'
-// import { Resend } from 'resend'
-
-// const resend = new Resend(process.env.RESEND_API_KEY)
-
-// export async function POST(request: Request) {
-//   try {
-//     const json = await request.json()
-
-//     const data = await resend.emails.send({
-//       from: 'Zaplanuj Podróż <onboarding@resend.dev>',
-//       to: ['biuro.zaplanujpodroz@gmail.com'],
-//       subject: 'Zapytanie ze strony www',
-//       react: EmailTemplate(json),
-//     })
-
-//     return NextResponse.json(json)
-//   } catch (error) {
-//     return NextResponse.json({ error })
-//   }
-// }
-
 import { NextResponse } from 'next/server'
 import { sendEmail } from '@/lib/mailer'
 
