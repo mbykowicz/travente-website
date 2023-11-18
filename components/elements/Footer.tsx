d88042e6364277dc1b30cab48b0296b67cafabff
Mateusz Bykowicz <mbykowicz@gmail.com>
2023-11-18 21:17:16 +0100

    chore(refactor): extract footer component to new file

--- components/elements/Footer.tsx
+++ components/elements/Footer.tsx
@@ -1,6 +1,6 @@
 import Logo from '@/components/Logo'
 
-function Footer() {
+export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center gap-4 my-12 text-xs text-storm-dust-600">
       <Logo className="h-8" />
