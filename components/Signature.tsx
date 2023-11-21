eac1998e37b98f5551b703e704336f1ed0c22a70
Mateusz Bykowicz <mbykowicz@gmail.com>
2023-11-21 19:37:21 +0100

    feat(styles): change font

--- components/Signature.tsx
+++ components/Signature.tsx
@@ -9,7 +9,7 @@ function Signature({
     <div className="my-6">
       <p className="font-script text-8xl text-brandy-punch-700">{name}</p>
       {description && (
-        <p className="-mt-3 font-sans text-sm text-storm-dust-600">
+        <p className="-mt-3 font-serif text-sm text-storm-dust-600">
           {description}
         </p>
       )}
