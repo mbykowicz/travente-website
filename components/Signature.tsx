d62414fa905aaed92c016bb62288e97c7e260e12
Mateusz Bykowicz <mbykowicz@gmail.com>
2023-11-21 19:45:20 +0100

    fix: signature`s name font style

--- components/Signature.tsx
+++ components/Signature.tsx
@@ -7,7 +7,9 @@ function Signature({
 }) {
   return (
     <div className="my-6">
-      <p className="font-script text-8xl text-brandy-punch-700">{name}</p>
+      <p className="font-script not-italic text-8xl text-brandy-punch-700">
+        {name}
+      </p>
       {description && (
         <p className="-mt-3 font-serif text-sm text-storm-dust-600">
           {description}
